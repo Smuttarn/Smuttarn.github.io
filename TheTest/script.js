9b1247a6d1e737f94d947f32902c20b3a757c793
Pablo Ahava Pizarro <66680173+Smuttarn@users.noreply.github.com>
2021-05-13 19:22:36 +0200

    Fixing struff

--- TheTest/script.js
+++ TheTest/script.js
@@ -1,17 +1,59 @@
-
-
-//Testing jQuery
+var clicks = 0;
 var hasClicked = true;
-$("#testButton").click(function(){
-    if(hasClicked){
-        $("#test").text("Something");
-        hasClicked = false;
-        return;
-    }
+var timestamp;
 
-    $("#test").text("Nothing");
-    hasClicked = true;
+$("#redButton").click(function(){
+    clicks++;
+    clickCheck();
 })
+
+window.onload = function(){
+    getTimestamp();
+}
+
+function getTimestamp(){
+    timestamp = Date.now();
+}
+
+function changeText(text){
+    $("#feedbackText").text(text);
+}
+
+function milliToSec(millis){
+    return Math.floor(millis/1000);
+}
+
+function getWastedTime(){
+    var output = Date.now() - timestamp;
+    alert(output);
+    return milliToSec(output);
+}
+
+function clickCheck(){
+
+    switch(clicks){
+    case 1: case 3: case 7:
+        changeText("Something");
+        break;
+    case 2: case 4: case 8:
+        changeText("Nothing");
+        break;
+    case 5:
+        changeText("What are you doing?");
+        break;
+    case 6:
+        changeText("...");
+        break;
+    case 9:
+        changeText("I mean...");
+        break;
+    case 10:
+        changeText("...what are you trying to accomplish?");
+        break;
+    case 11:
+        changeText("You've already wasted " + getWastedTime() + " seconds of your life here.")
+    }
+}
 
 
 
