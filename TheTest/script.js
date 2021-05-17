046e678d165a2e0276b7eb7332db5fd22785d528
Pablo Ahava Pizarro <66680173+Smuttarn@users.noreply.github.com>
2021-05-17 18:35:59 +0200

    Pimpin'

--- TheTest/script.js
+++ TheTest/script.js
@@ -1,6 +1,9 @@
 var clicks = 0;
-var hasClicked = true;
+var hasClicked = false;
 var timestamp;
+
+
+
 
 $("#redButton").click(function(){
     clicks++;
@@ -9,14 +12,45 @@ $("#redButton").click(function(){
 
 window.onload = function(){
     getTimestamp();
+    //changeText("Nothing");
+
+    openingScene();
+}
+
+
+function openingScene(){
+    $("#redButton").hide();
+    $("#feedbackText").hide();
+
+    $("#redButton").fadeIn(3000, function(){
+        $("#feedbackText").fadeIn(2000, function(){
+            $(this).text("Nothing");
+        });
+    });
+}
+
+
+function disableButton(){
+    $("#redButton").css("background-color", "red");
+    $("#redButton").css("border", "7px solid darkgray");
+}
+
+function enableButton(){
+    $("#redButton").css("background-color", "gray");
+    $("#redButton").css("border", "10px solid darkgray");
+}
+
+
+
+function changeText(text){
+    $("#feedbackText").fadeOut(500, function(){
+        disableButton();
+        $(this).text(text);
+    }).fadeIn(800, enableButton());
 }
 
 function getTimestamp(){
     timestamp = Date.now();
-}
-
-function changeText(text){
-    $("#feedbackText").text(text);
 }
 
 function milliToSec(millis){
@@ -25,23 +59,22 @@ function milliToSec(millis){
 
 function getWastedTime(){
     var output = Date.now() - timestamp;
-    alert(output);
     return milliToSec(output);
 }
 
 function clickCheck(){
 
     switch(clicks){
-    case 1: case 3: case 7:
+    case 1: case 3: case 7: case 11:
         changeText("Something");
         break;
-    case 2: case 4: case 8:
+    case 2: case 4: case 6: case 12:
         changeText("Nothing");
         break;
     case 5:
         changeText("What are you doing?");
         break;
-    case 6:
+    case 8:
         changeText("...");
         break;
     case 9:
@@ -50,8 +83,9 @@ function clickCheck(){
     case 10:
         changeText("...what are you trying to accomplish?");
         break;
-    case 11:
-        changeText("You've already wasted " + getWastedTime() + " seconds of your life here.")
+    case 13:
+        changeText("You've already wasted " + getWastedTime() + " seconds of your life..");
+        break;
     }
 }
 
